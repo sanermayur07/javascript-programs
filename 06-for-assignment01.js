6145130ef81fad6f24c9dd678005db7340f4a314
sanermayur07 <mayurpatil5057@gmail.com>
2024-08-10 23:24:57 +0530

    Control Flow-for Loop
    Control Flow-for Loop

---
+++ 06-for-assignment01.js
@@ -0,0 +1,40 @@
+console.log("============Print Numbers from 5 to 15 by Incrementing 1============");
+for (let index = 5; index <=15 ; index = index+1) {
+    console.log(index);
+}
+
+
+console.log("============Print Numbers from 50 to 40 by Decrementing 1============");
+for (let index = 50; index >=40; index--) {
+   console.log(index);
+}
+
+
+console.log("============================Find 15 Odd Numbers========================");
+for (let index = 1; index <=30; index = index+2) {
+    console.log(index);
+}
+
+
+console.log("============================Find 10 Even Numbers========================");
+for (let index = 2; index <= 20; index = index+2) {
+   console.log(index);
+}
+
+
+console.log("============================Print table of 5========================");
+for (let index = 5; index <= 50; index = index+5) {
+  console.log(index);
+}
+
+
+console.log("============================Print table of 10========================");
+for (let index = 10; index <=100 ; index = index+10) {
+   console.log(index);
+}
+
+
+console.log("============================Print table of 10 in reverse order============");
+for (let index = 100; index >=10 ; index=index-10) {
+   console.log(index);
+}
